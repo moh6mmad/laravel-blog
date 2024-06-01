2ce78377365ffca96723fc87bba1d20cb4aab202
mohamm6d <mohammad@lync.me>
2024-06-01 20:35:11 +0200

    updated README, added publishable config file

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,16 +6,29 @@ use Moh6mmad\LaravelBlog\Console\InstallLaravelBlog;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    /**
+     * Get the config path
+     *
+     * @return string
+     */
+    protected function getConfigPath()
+    {
+        return config_path('laravel-blog.php');
+    }
+
     public function register()
     {
         $this->app->make('Moh6mmad\LaravelBlog\Http\Controllers\LaravelBlogController');
         $this->app->make('Moh6mmad\LaravelBlog\Http\Models\LaravelBlog');
-        $this->loadViewsFrom(__DIR__.'/views', 'laravel-settings');
+        $this->loadViewsFrom(__DIR__ . '/views', 'laravel-settings');
     }
 
     public function boot()
     {
+        $configPath = __DIR__ . '/config/laravel-blog.php';
+        $this->publishes([$configPath => $this->getConfigPath()], 'config');
+
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        include __DIR__.'/routes/routes.php';
+        include __DIR__ . '/routes/routes.php';
     }
 }
